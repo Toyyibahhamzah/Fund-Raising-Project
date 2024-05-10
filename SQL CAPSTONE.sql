fce81cc69de27dbd46adbaf5b7137301e93870f1
TOYYIBAH HAMZAH OLABODE <159651133+Toyyibahhamzah@users.noreply.github.com>
2024-05-10 21:26:04 +0100

    Update SQL CAPSTONE.sql

--- SQL CAPSTONE.sql
+++ SQL CAPSTONE.sql
@@ -1,5 +1,4 @@
--- Question 1
--- How much is the total donation?
+-- Total donation
 
 SELECT sum(donation) as Total_donation
 from donation_data;
@@ -8,23 +7,20 @@ SELECT TO_CHAR(SUM(donation), '999,999,999') AS Total_donation
 FROM donation_data;
 
 
--- Question 2
--- What is the total donation by gender?
+-- Total donation by gender
 
 select gender,TO_CHAR(SUM(donation), '999,999,999') as Total_Don_Gender
 from donation_data
 group by gender;
 
 
--- Question 3
--- Show the total donation and number of donations by gender
+-- Total donation and number of donations by gender
 
 select gender, count(id) as No_of_Donations,TO_CHAR(SUM(donation), '999,999,999') as Total_Don_Gender
 from donation_data
 group by gender;
 
 
--- Question 4
 -- Total donation made by frequency of donation
 
 select donation_frequency from donor_data;
@@ -36,7 +32,6 @@ join donation_data d on d.id=dd.id
 group by donation_frequency;
 
 
--- Question 5
 -- Total donation and number of donation by Job field
 SELECT job_field, 
 TO_CHAR(SUM(donation), '999,999,999') AS Total_donation,
@@ -45,7 +40,6 @@ from donation_data
 group by job_field;
 
 
--- Question 6
 -- Total donation and number of donations above $200
 select id,donation from donation_data
 where donation > 200;
@@ -56,7 +50,6 @@ from donation_data
 where donation > 200;
 
 
--- Question 7
 -- Total donation and number of donations below $200
 
 select id,donation from donation_data
@@ -68,8 +61,7 @@ from donation_data
 where donation < 200;
 
 
--- Question 8
--- Which top 10 states contributes the highest donations
+-- Top 10 states that contributes the highest donations
 
 select state, 
 TO_CHAR(SUM(donation), '999,999,999') AS Total_donation
@@ -80,7 +72,7 @@ limit 10;
 
 
 -- Question 9
--- Which top 10 states contributes the least donations
+-- Top 10 states that contributes the least donations
 
 select state, 
 TO_CHAR(SUM(donation), '999,999,999') AS Total_donation
@@ -90,8 +82,7 @@ order by Total_donation asc
 limit 10;
 
 
--- Question 10 
--- What are the top 10 cars driven by the highest donors
+-- The top 10 cars driven by the highest donors
 
 SELECT dd.car, 
 TO_CHAR(SUM(d.donation), '999,999,999') AS Total_donation
@@ -102,7 +93,6 @@ order by Total_donation desc
 limit 10;
 
 
--- Additional Insights
 -- Patterns in donation frequency based on donor demographics (e.g., gender, age)?
 
 SELECT dd.donation_frequency, 
@@ -110,9 +100,6 @@ TO_CHAR(SUM(d.donation), '999,999,999') AS Total_donation, count(d.gender), d.ge
 from donor_data dd
 join donation_data d on d.id=dd.id
 group by donation_frequency,gender;
-
-
-
 
 
 SELECT 
